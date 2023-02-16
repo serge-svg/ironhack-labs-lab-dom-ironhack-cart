234ecfdb9696f339ac116bc38acfe855967feb39
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-02-16 16:05:41 +0100

    Version with calculateAll

---
+++ tests/index.spec.js
@@ -0,0 +1,11 @@
+describe('Find the maximum', () => {
+  /*
+  it('should declare a function named updateSubtotal', () => {
+    expect(typeof updateSubtotal).toBe('function');
+  });
+  
+  it('should return a number', () => {
+    expect(typeof updateSubtotal(1)).toEqual('number');
+  });
+  */
+});
